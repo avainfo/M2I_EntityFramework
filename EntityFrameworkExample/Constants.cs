9e2365ae8703ce150507761a9808810fff9e27b8
anton <avainfoconseils@gmail.com>
2025-02-10 15:29:49 +0100

    Added parameters password for launching app

--- EntityFrameworkExample/Constants.cs
+++ EntityFrameworkExample/Constants.cs
@@ -2,6 +2,7 @@
 
 public static class Constants
 {
+	public static string ConnectionPassword = "";
 	public static string ConnectionString =
-		"Server=AVAP\\SQLEXPRESS;Database=master;Trusted_Connection=True;User Id=sa;Password=root; TrustServerCertificate=true";
+		$"Server=AVAP\\SQLEXPRESS;Database=master;Trusted_Connection=True;User Id=sa;Password={ConnectionPassword}; TrustServerCertificate=true";
 }
--- EntityFrameworkExample/Program.cs
+++ EntityFrameworkExample/Program.cs
@@ -2,11 +2,49 @@
 
 public class Program
 {
+	/**
+	 * Launch app with ./EntityFrameworkExample -p [password]
+	 */
 	public static void Main(string[] args)
 	{
+		if (args is not ["-p", _])
+		{
+			Console.WriteLine("Usage: ./EntityFrameworkExample -p <password>");
+			return;
+		}
+
+		Constants.ConnectionPassword = args[1];
+
 		using (AppDbContext context = new AppDbContext())
 		{
-			Console.WriteLine("Hello World!");
+			var orders = context.Orders.ToList();
+
+			foreach (Order order in orders)
+			{
+				Console.WriteLine($"Commande de {order.ProductName} pour l'utilisateur : {order.CustomerId}.");
+			}
+
+			Console.WriteLine("====================");
+			var firstOrder = context.Orders.FirstOrDefault(order => order.DeliveryGuyId == 2);
+			Console.WriteLine($"Heure de la première livraison: {firstOrder.OrderDate.TimeOfDay}.");
+
+			Console.WriteLine("====================");
+			orders = context.Orders.OrderBy(order => order.OrderDate).ToList();
+
+			foreach (Order order in orders)
+			{
+				Console.WriteLine($"Commande de {order.ProductName} pour l'utilisateur : {order.CustomerId}.");
+			}
+
+			Console.WriteLine("====================");
+			var customOrders = context.Orders
+				.OrderBy(order => order.OrderDate)
+				.Select(order => new { order.ProductName, order.Description })
+				.ToList();
+			foreach (var customOrder in customOrders)
+			{
+				Console.WriteLine($"Commande de {customOrder.ProductName} : {customOrder.Description}.");
+			}
 		}
 	}
 }
